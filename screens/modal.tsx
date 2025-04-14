932387026f3b48a9edfb33a09b0931de9044cf4f
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-04-14 16:32:23 -0300

    Initial commit
    Generated by create-expo-stack 2.14.3

---
+++ screens/modal.tsx
@@ -0,0 +1,15 @@
+import { InternalizationExample } from 'components/InternalizationExample';
+import { ScreenContent } from 'components/ScreenContent';
+import { StatusBar } from 'expo-status-bar';
+import { Platform } from 'react-native';
+
+export default function Modal() {
+  return (
+    <>
+      <ScreenContent path="screens/modal.tsx" title="Modal">
+        <InternalizationExample />
+      </ScreenContent>
+      <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
+    </>
+  );
+}
