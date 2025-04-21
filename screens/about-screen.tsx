c47a0bf8868bfd8b10cd96d9e89f7037fd8bab26
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-04-21 16:57:24 -0300

    feat: adds logout functionality

--- screens/about-screen.tsx
+++ screens/about-screen.tsx
@@ -1,8 +1,32 @@
 import React from 'react';
+import { useAuth } from 'store/auth-store';
+import { Button, ButtonText } from 'tamagui';
 import { ScreenContent } from '../components/ScreenContent';
 
 const About: React.FC = () => {
-  return <ScreenContent path="screens/about-screen" title="About Screen" />;
+  // TO DO: REMOVE THIS BUTTON and replace this later by firebase auth
+  const signOut = useAuth((state) => state.signOut);
+  const handlePress = () => {
+    signOut();
+  };
+
+  return (
+    <ScreenContent path="screens/about-screen" title="About Screen">
+      <Button
+        size="$4"
+        onPress={handlePress}
+        theme="alt1"
+        backgroundColor="$background"
+        color="$color"
+        borderWidth={1}
+        borderColor="$borderColor"
+        borderRadius="$2"
+        padding="$2"
+        margin="$2">
+        <ButtonText>Logout</ButtonText>
+      </Button>
+    </ScreenContent>
+  );
 };
 
 export default About;
