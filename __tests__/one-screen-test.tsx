9b17cd970e7c9a5b25cf2cfbb19892fc8d63db3d
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-04-22 14:53:20 -0300

    test: adds first test

--- __tests__/one-screen-test.tsx
+++ __tests__/one-screen-test.tsx
@@ -2,8 +2,8 @@ import { render } from '@testing-library/react-native';
 
 // TO DO: Configure TamaguiProvider for testing
 import { TamaguiProvider } from 'tamagui';
-import config from '../tamagui.config';
 import TabOneScreen from '../screens/one';
+import config from '../tamagui.config';
 
 const Provider = ({ children }: { children: React.ReactNode }) => (
   <TamaguiProvider config={config}>{children}</TamaguiProvider>
@@ -11,11 +11,11 @@ const Provider = ({ children }: { children: React.ReactNode }) => (
 
 describe('TabOne Screen', () => {
   it('renders correctly', () => {
-    const component = render(
+    const view = render(
       <Provider>
         <TabOneScreen />
       </Provider>
     );
-    expect(component).toMatchSnapshot();
+    expect(view).toMatchSnapshot();
   });
 });
