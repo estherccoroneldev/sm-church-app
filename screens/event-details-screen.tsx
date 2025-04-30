e1828fd6bfe5c52fbc8717b30a443f582db182cc
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-04-30 18:55:33 -0300

    feat: adds the Event Details Screen

--- screens/event-details-screen.tsx
+++ screens/event-details-screen.tsx
@@ -1,21 +1,15 @@
 import { RouteProp, useRoute } from '@react-navigation/native';
 import { EventsParamList } from 'navigation/tab-navigator';
 import React from 'react';
-import { ScreenContent } from '../components/ScreenContent';
+import DetailsLayout from '../components/DetailsLayout';
 
 type EventDetailsRouteProp = RouteProp<EventsParamList, 'EventDetails'>;
 
 const EventDetails: React.FC = () => {
   const { params } = useRoute<EventDetailsRouteProp>();
+  const { imageUrl, contact = 'Gary Oldman' } = params || {};
 
-  // Extract the eventId from params
-  const eventId = params?.id;
-  // Log the eventId to verify it's being passed correctly
-  console.log('Event ID:', eventId);
-  const { title, description, date } = params || {};
-  console.log('Event Details:', { title, description, date });
-
-  return <ScreenContent path="screens/event-details-screen" title="EventDetails Screen" />;
+  return <DetailsLayout currentDetail={{ imageUrl, contact, ...params }} />;
 };
 
 export default EventDetails;
