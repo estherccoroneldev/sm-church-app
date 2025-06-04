28de6d1bbab0f5f8dbdd1b5f59b0a7d24d4b02de
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-06-04 17:24:32 -0300

    feat: adds welcome screen and register to the auth stack navigator

--- navigation/auth-navigator.tsx
+++ navigation/auth-navigator.tsx
@@ -1,23 +1,26 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Register from 'screens/register-screen';
+import Welcome from 'screens/welcome-screen';
 import ForgotPassword from '../screens/forgot-password-screen';
 import Login from '../screens/login-screen';
 
 export type AuthStackParamList = {
   Login: undefined;
   ForgotPassword: undefined;
+  Welcome: undefined;
+  Register: undefined;
+  RegisterWithPhone: undefined;
 };
 
 const Stack = createNativeStackNavigator<AuthStackParamList>();
 
 export default function AuthNavigator() {
   return (
-    <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-      <Stack.Screen
-        name="ForgotPassword"
-        component={ForgotPassword}
-        options={{ headerShown: false }}
-      />
+    <Stack.Navigator initialRouteName="Welcome" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="Welcome" component={Welcome} />
+      <Stack.Screen name="Login" component={Login} />
+      <Stack.Screen name="Register" component={Register} />
+      <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
     </Stack.Navigator>
   );
 }
