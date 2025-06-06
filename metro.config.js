46bf0f14d207da22f8ecf2ee7ffd5ec6201e53c5
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-06-06 17:50:06 -0300

    chore: adds type rules

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,14 @@
 const { getDefaultConfig } = require('@expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
-config.resolver.sourceExts.push('cjs');
+// This is needed to support the `cjs` extension for CommonJS modules
+config.resolver.sourceExts = [...config.resolver.sourceExts, 'cjs'];
+
+// Disable package exports to avoid issues with certain packages (firebase/auth)
+// that may not be compatible with the package exports feature
+// This is particularly useful for packages that are not fully ESM compliant
+// or have specific requirements for CommonJS modules.
+// for more details, see: https://docs.expo.dev/guides/using-firebase/#configure-metro
 config.resolver.unstable_enablePackageExports = false;
 
 module.exports = config;
