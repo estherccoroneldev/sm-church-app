4596b335a6884d427b612448ded02c835274896a
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-07-01 11:34:40 -0300

    integrates Firestore w client-side

--- hooks/use-ministries.ts
+++ hooks/use-ministries.ts
@@ -1,7 +1,29 @@
 import React from 'react';
 
+import { db } from 'config/firebase';
+import { collection, getDocs } from 'firebase/firestore';
 import { Ministry } from '../@types/ministry';
 import fetchMinistries from '../services/ministry';
+
+async function fetchMinistriesGCP(): Promise<Ministry[]> {
+  try {
+    const querySnapshot = await getDocs(collection(db, 'ministries'));
+    const ministriesList = querySnapshot.docs.map(
+      (doc) =>
+        ({
+          id: doc.id,
+          ...doc.data(),
+        }) as Ministry
+    );
+    return ministriesList;
+  } catch (error) {
+    console.error('[Firestore] Error fetching ministries:', error);
+    throw error;
+  }
+}
+
+// Function to determine which fetch function to use based on the environment
+const getData = __DEV__ ? fetchMinistries : fetchMinistriesGCP;
 
 export default function useMinistries() {
   const [ministries, setMinistries] = React.useState<Ministry[]>([]);
@@ -11,7 +33,7 @@ export default function useMinistries() {
   const getMinistries = React.useCallback(async () => {
     try {
       setLoading(true);
-      const data = await fetchMinistries();
+      const data = await getData();
 
       setMinistries(data);
       setLoading(false);
