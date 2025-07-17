bc5471caac28e0bc5495f92580bb2e9aa570e2bd
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-07-17 18:30:35 -0300

    splits fetch events from GCP out

--- services/events.ts
+++ services/events.ts
@@ -1,3 +1,5 @@
+import { db } from 'config/firebase';
+import { collection, getDocs } from 'firebase/firestore';
 import { Event } from '../@types/event';
 import api from './api';
 
@@ -11,4 +13,23 @@ const fetchEvents = async (): Promise<Event[]> => {
   }
 };
 
-export default fetchEvents;
+async function getEventsFromFirestore(): Promise<Event[]> {
+  try {
+    const querySnapshot = await getDocs(collection(db, 'events'));
+    const eventsList = querySnapshot.docs.map(
+      (doc) =>
+        ({
+          id: doc.id,
+          ...doc.data(),
+        }) as Event
+    );
+    return eventsList;
+  } catch (error) {
+    console.error('[Firestore] Error fetching events:', error);
+    throw error;
+  }
+}
+
+const getData = __DEV__ ? fetchEvents : getEventsFromFirestore;
+
+export { getData };
