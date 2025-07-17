23cb353665233ccf12d2313499ecce30e147797b
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-07-17 18:16:12 -0300

    refactor use ministries

--- hooks/use-ministries.ts
+++ hooks/use-ministries.ts
@@ -1,39 +1,17 @@
 import React from 'react';
 
-import { db } from 'config/firebase';
-import { collection, getDocs } from 'firebase/firestore';
+import { getMinistries } from 'services/ministry';
 import { Ministry } from '../@types/ministry';
-import fetchMinistries from '../services/ministry';
-
-async function fetchMinistriesGCP(): Promise<Ministry[]> {
-  try {
-    const querySnapshot = await getDocs(collection(db, 'ministries'));
-    const ministriesList = querySnapshot.docs.map(
-      (doc) =>
-        ({
-          id: doc.id,
-          ...doc.data(),
-        }) as Ministry
-    );
-    return ministriesList;
-  } catch (error) {
-    console.error('[Firestore] Error fetching ministries:', error);
-    throw error;
-  }
-}
-
-// Function to determine which fetch function to use based on the environment
-const getData = __DEV__ ? fetchMinistries : fetchMinistriesGCP;
 
 export default function useMinistries() {
   const [ministries, setMinistries] = React.useState<Ministry[]>([]);
   const [loading, setLoading] = React.useState<boolean>(true);
   const [error, setError] = React.useState<string | null>(null);
 
-  const getMinistries = React.useCallback(async () => {
+  const fetchMinistries = React.useCallback(async () => {
     try {
       setLoading(true);
-      const data = await getData();
+      const data = await getMinistries();
 
       setMinistries(data);
       setLoading(false);
@@ -46,11 +24,11 @@ export default function useMinistries() {
   }, []);
 
   React.useEffect(() => {
-    getMinistries();
-  }, [getMinistries]);
+    fetchMinistries();
+  }, [fetchMinistries]);
 
   return React.useMemo(
-    () => ({ ministries, loading, error, getMinistries }),
-    [loading, error, ministries, getMinistries]
+    () => ({ ministries, loading, error, fetchMinistries }),
+    [loading, error, ministries, fetchMinistries]
   );
 }
