4095990ae056bf8a4f9c66441502ebaa8b1427f3
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-07-17 18:15:47 -0300

    splits ministries out

--- services/ministry.ts
+++ services/ministry.ts
@@ -1,7 +1,9 @@
+import { db } from 'config/firebase';
+import { collection, getDocs } from 'firebase/firestore';
 import { Ministry } from '../@types/ministry';
 import api from './api';
 
-const fetchMinistries = async (): Promise<Ministry[]> => {
+export const fetchMinistries = async (): Promise<Ministry[]> => {
   try {
     const response = await api.get('/ministries');
     return response.data;
@@ -11,4 +13,23 @@ const fetchMinistries = async (): Promise<Ministry[]> => {
   }
 };
 
-export default fetchMinistries;
+export async function fetchMinistriesGCP(): Promise<Ministry[]> {
+  try {
+    const querySnapshot = await getDocs(collection(db, 'ministries'));
+    const ministriesList = querySnapshot.docs.map(
+      (doc) =>
+        ({
+          id: doc.id,
+          ...doc.data(),
+        }) as Ministry
+    );
+    return ministriesList;
+  } catch (error) {
+    console.error('[Firestore] Error fetching ministries:', error);
+    throw error;
+  }
+}
+
+// Function to determine which fetch function to use based on the environment
+const getMinistries = __DEV__ ? fetchMinistries : fetchMinistriesGCP;
+export { getMinistries };
