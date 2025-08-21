fcb060efcf4288a0e1a5eef37abeaccaff31fb97
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-08-21 16:00:01 -0300

    adds new field phoneNumber to the user schema

--- @types/user.ts
+++ @types/user.ts
@@ -3,6 +3,7 @@ import { Timestamp } from 'firebase/firestore';
 export type UserProfile = {
   uid: string;
   email: string;
+  phoneNumber?: string;
   firstName: string;
   lastName: string;
   role: 'admin' | 'member' | 'apprentice'; // Default roles
