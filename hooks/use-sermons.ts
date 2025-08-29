e100be1fc762cc71c93424314c70fac63fe1af4b
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-08-29 12:58:30 -0300

    updates useSermons hook

--- hooks/use-sermons.ts
+++ hooks/use-sermons.ts
@@ -1,20 +1,33 @@
+import uniqBy from 'lodash/uniqBy';
 import React from 'react';
 
-import { Sermon } from '../@types/sermon';
-import fetchSermons from '../services/sermons';
+import { YouTubeVideo } from '@types/ytvideo';
+import { fetchVideos } from 'services/ytvideos';
+// import { Sermon } from '../@types/sermon';
+// import fetchSermons from '../services/sermons';
 
 export default function useSermons() {
-  const [sermons, setSermons] = React.useState<Sermon[]>([]);
-  const [loading, setLoading] = React.useState<boolean>(true);
+  // const [sermons, setSermons] = React.useState<Sermon[]>([]);
+  const [sermons, setSermons] = React.useState<YouTubeVideo[]>([]);
+  const [nextPageToken, setNextPageToken] = React.useState<string>('');
+  const [loading, setLoading] = React.useState<boolean>(false);
   const [error, setError] = React.useState<string | null>(null);
 
   const getSermons = React.useCallback(async () => {
+    if (loading || (!nextPageToken && sermons.length > 0)) return;
     try {
+      // const data = await fetchSermons();
+      // setSermons(data);
       setLoading(true);
-      const data = await fetchSermons();
 
-      setSermons(data);
-      setLoading(false);
+      const data = await fetchVideos(nextPageToken);
+
+      if (data) {
+        // Prevent duplicates
+        setSermons((prevSermons) => uniqBy([...prevSermons, ...data.items], 'id.videoId'));
+
+        setNextPageToken(data.nextPageToken);
+      }
     } catch (error) {
       setError('Error fetching sermons');
       console.error('Error fetching sermons:', JSON.stringify(error));
@@ -25,7 +38,7 @@ export default function useSermons() {
 
   React.useEffect(() => {
     getSermons();
-  }, [getSermons]);
+  }, []);
 
   return React.useMemo(
     () => ({ sermons, loading, error, getSermons }),
