9ebe26f57d42a14f49876a94ff5314197f040d15
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-09-16 20:53:50 -0300

    adds announcements service and hook

---
+++ hooks/use-announcements.ts
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { getData } from 'services/announcements';
+import { Ministry } from '../@types/ministry';
+
+export default function useAnnouncements() {
+  const [announcements, setAnnouncements] = React.useState<Ministry[]>([]);
+  const [loading, setLoading] = React.useState<boolean>(true);
+  const [error, setError] = React.useState<string | null>(null);
+
+  const fetchAnnouncements = React.useCallback(async () => {
+    try {
+      setLoading(true);
+      const data = await getData();
+
+      setAnnouncements(data);
+      setLoading(false);
+    } catch (error) {
+      setError('Error fetching Announcements');
+      console.error('Error fetching Announcements:', JSON.stringify(error));
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  React.useEffect(() => {
+    fetchAnnouncements();
+  }, []);
+
+  return React.useMemo(
+    () => ({ announcements, loading, error, fetchAnnouncements }),
+    [loading, error, announcements, fetchAnnouncements]
+  );
+}
---
+++ services/announcements.ts
@@ -0,0 +1,51 @@
+import { db } from 'config/firebase';
+import { format } from 'date-fns';
+import { collection, getDocs } from 'firebase/firestore';
+import { Event } from '../@types/event';
+import api from './api';
+
+const fetchAnnouncements = async (): Promise<Event[]> => {
+  try {
+    const response = await api.get('/announcements');
+    return response.data;
+  } catch (error) {
+    console.error('[API] Error fetching announcements:', error);
+    throw error;
+  }
+};
+
+const setFormattedDate = (docData: any): string => {
+  const firebaseTimestamp = docData.date;
+
+  let formattedDate = 'N/A';
+  if (firebaseTimestamp && typeof firebaseTimestamp.toDate === 'function') {
+    const jsDate = firebaseTimestamp.toDate();
+    formattedDate = format(jsDate, 'MMMM d, yyyy');
+  }
+
+  return formattedDate;
+};
+
+async function getAnnouncementsFromFirestore(): Promise<Event[]> {
+  try {
+    const querySnapshot = await getDocs(collection(db, 'announcements'));
+    const announcementsList = querySnapshot.docs.map(
+      (doc) =>
+        ({
+          id: doc.id,
+          ...doc.data(),
+          date: setFormattedDate(doc.data()),
+        }) as Event
+    );
+
+    return announcementsList;
+  } catch (error) {
+    console.error('[Firestore] Error fetching announcements:', error);
+    throw error;
+  }
+}
+
+const getData = getAnnouncementsFromFirestore;
+// const getData = __DEV__ ? fetchAnnouncements : getAnnouncementsFromFirestore;
+
+export { getData };
