61013ae6482e52f2a5144f139c0e1ac8ed9f6fbb
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-09-18 13:52:02 -0300

    adds ministries selection to the root stack

--- navigation/root-stack-navigator.tsx
+++ navigation/root-stack-navigator.tsx
@@ -1,23 +1,46 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 // import { Platform } from 'react-native';
 // import Modal from '../screens/modal';
-import TabNavigator, { TabParamList } from './tab-navigator';
+import MinistriesSelectionScreen from 'screens/ministries-selection-screen';
+import { useAuth } from 'store/auth-store';
+import TabNavigator from './tab-navigator';
 
 export type RootStackParamList = {
-  TabNavigator: TabParamList;
+  TabNavigator: undefined;
   Modal: undefined;
+  MinistriesSelection: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function RootStack() {
+  const authUser = useAuth((state) => state.user);
+
   return (
-    <Stack.Navigator initialRouteName="TabNavigator">
+    <Stack.Navigator initialRouteName={'TabNavigator'}>
       <Stack.Group
         screenOptions={{
           headerShown: false,
         }}>
-        <Stack.Screen name="TabNavigator" component={TabNavigator} />
+        {authUser?.hasSelectedMinistries === 'no' && authUser?.role === 'member' ? (
+          <Stack.Screen
+            name="MinistriesSelection"
+            component={MinistriesSelectionScreen}
+            options={{
+              headerShown: true,
+              headerTitle: 'Registro',
+              headerTitleAlign: 'center',
+              headerShadowVisible: false,
+              headerBackButtonDisplayMode: 'minimal',
+              headerTitleStyle: {
+                fontSize: 24,
+                fontFamily: 'Outfit_600SemiBold',
+              },
+            }}
+          />
+        ) : (
+          <Stack.Screen name="TabNavigator" component={TabNavigator} />
+        )}
       </Stack.Group>
       {/* <Stack.Group
         screenOptions={{
