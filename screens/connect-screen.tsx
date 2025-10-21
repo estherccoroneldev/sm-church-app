2f59b8475962730c2cbf4d4d7e2bed0ede57629e
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-10-21 20:33:34 -0300

    fixes connect type title

--- screens/connect-screen.tsx
+++ screens/connect-screen.tsx
@@ -16,26 +16,26 @@ import { ConnectParamList } from '../navigation/tab-navigator';
 const connectItems = [
   {
     id: 'item-1',
-    name: 'Conéctate con nosotros',
+    title: 'Conéctate con nosotros',
     description: 'Haznos saber cómo podemos apoyarte.',
     imageUrl: 'https://www.iglesiasanmateo.org/uploads/5/6/5/4/56543779/dsc-4296_orig.jpg',
   },
   {
     id: 'item-2',
-    name: 'Encuentra un grupo',
+    title: 'Encuentra un grupo',
     description: 'Encuentra tu lugar uniéndote a uno de nuestros grupos',
     imageUrl:
       'https://www.iglesiasanmateo.org/uploads/5/6/5/4/56543779/conferencia-lideres-hispanos-2023-2_1.jpg',
   },
   {
     id: 'item-3',
-    name: 'Oportunidades de Servicio',
+    title: 'Oportunidades de Servicio',
     description: 'Encuentra tu lugar sirviendo en la iglesia, en la ciudad o en el mundo',
     imageUrl: 'https://www.iglesiasanmateo.org/uploads/5/6/5/4/56543779/dsc-8256_orig.jpg',
   },
   {
     id: 'item-4',
-    name: 'Sobre nosotros',
+    title: 'Sobre nosotros',
     description: '',
     // TO DO: replace this image by the actual church front
     imageUrl:
