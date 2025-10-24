b648848b8fd3a738b13c537fc90903dec4e2083f
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-10-24 15:25:14 -0300

    updates firebase config

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,36 +1,43 @@
-import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
-import { getApp, getApps, initializeApp } from 'firebase/app';
-import {
-  // getAuth,
-  getReactNativePersistence,
-  initializeAuth,
-} from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
+import firebase from '@react-native-firebase/app';
+import firestore from '@react-native-firebase/firestore';
 
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
-  projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
-  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
-};
+export const db = firestore();
 
-let app;
-if (getApps().length === 0) {
-  app = initializeApp(firebaseConfig);
-} else {
-  app = getApp();
-}
+export default firebase;
 
-// const auth = getAuth(app);
-const auth = initializeAuth(app, {
-  persistence: getReactNativePersistence(ReactNativeAsyncStorage),
-});
+// import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
+// import { getApp, getApps, initializeApp } from 'firebase/app';
+// import {
+//   // getAuth,
+//   getReactNativePersistence,
+//   initializeAuth,
+// } from 'firebase/auth';
+// import { getFirestore } from 'firebase/firestore';
 
-const db = getFirestore(app);
+// // Initialize Firebase
+// const firebaseConfig = {
+//   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+//   authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+//   projectId: process.env.EXPO_PUBLIC_PROJECT_ID,
+//   storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+//   appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+//   measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID,
+// };
 
-export { app, auth, db };
+// let app;
+// if (getApps().length === 0) {
+//   app = initializeApp(firebaseConfig);
+// } else {
+//   app = getApp();
+// }
+
+// // const auth = getAuth(app);
+// const auth = initializeAuth(app, {
+//   persistence: getReactNativePersistence(ReactNativeAsyncStorage),
+// });
+
+// const db = getFirestore(app);
+
+// export { app, auth, db };
