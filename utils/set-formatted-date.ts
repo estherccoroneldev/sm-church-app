bc5ec479e7a731ed0951c08215ba80c465d71d24
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-11-06 17:23:57 -0300

    fixes time zoned date

--- utils/set-formatted-date.ts
+++ utils/set-formatted-date.ts
@@ -1,12 +1,14 @@
 import { Timestamp } from '@react-native-firebase/firestore';
 import { format } from 'date-fns';
+import { toZonedTime } from 'date-fns-tz';
 
 export const setFormattedDate = (date: Timestamp): string => {
+  const userTimeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
   let formattedDate = 'N/A';
   if (date && typeof date.toDate === 'function') {
     const jsDate = date.toDate();
-    formattedDate = format(jsDate, 'PPpp');
-    // formattedDate = format(jsDate, 'MMMM d, yyyy');
+    const localDate = toZonedTime(jsDate, userTimeZone);
+    formattedDate = format(localDate, 'PPpp');
   }
 
   return formattedDate;
