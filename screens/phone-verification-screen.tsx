a5185179e280f2e11362cca25237c7eb7d3a4554
Leomariyer Coronel <lestherccoronel@gmail.com>
2025-11-07 16:30:45 -0300

    updates phone verification screen

--- screens/phone-verification-screen.tsx
+++ screens/phone-verification-screen.tsx
@@ -1,20 +1,22 @@
 import { getAuth, PhoneAuthProvider, verifyPhoneNumber } from '@react-native-firebase/auth';
-import { RouteProp, useRoute } from '@react-navigation/native';
+import { NavigationProp, RouteProp, useNavigation, useRoute } from '@react-navigation/native';
 import TextField from 'components/TextField';
-import { AuthStackParamList } from 'navigation/auth-navigator';
+import { db } from 'config/firebase';
+import { RootStackParamList } from 'navigation/root-stack-navigator';
 import React from 'react';
 import { Alert } from 'react-native';
-import { useAuth } from 'store/auth-store';
-import { Button, SizableText, useTheme, YStack } from 'tamagui';
+import { useAuthStore } from 'store/auth-store';
+import { SizableText, useTheme, YStack } from 'tamagui';
 import { PrimaryButton } from 'tamagui.config';
 
 const timeoutInMilliseconds = 60000;
-type PhoneVerificationScreenRouteProp = RouteProp<AuthStackParamList, 'PhoneVerificationScreen'>;
+type PhoneVerificationScreenRouteProp = RouteProp<RootStackParamList, 'PhoneVerificationScreen'>;
 
 const PhoneVerificationScreen: React.FC = () => {
   const { params } = useRoute<PhoneVerificationScreenRouteProp>();
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
   const theme = useTheme();
-  const signIn = useAuth((state) => state.signIn);
+  const signIn = useAuthStore((state) => state.signIn);
 
   const [confirm, setConfirm] = React.useState<any | null>(null);
   const [code, setCode] = React.useState('');
@@ -31,23 +33,34 @@ const PhoneVerificationScreen: React.FC = () => {
       const credential = PhoneAuthProvider.credential(confirm.verificationId, code);
       const currentUser = getAuth().currentUser;
       if (currentUser) {
-        let userData = await currentUser.linkWithCredential(credential);
+        const userDocRef = db.collection('users').doc(currentUser.uid);
+        const userCredential = await currentUser.linkWithCredential(credential);
+        await userDocRef.set(
+          {
+            isPhoneVerified: true,
+            phoneNumber: userCredential.user.phoneNumber,
+          },
+          { merge: true }
+        );
+        const userDoc = await userDocRef.get();
 
         signIn({
-          id: userData.user.uid,
-          name: `${userData.user.displayName ?? ''}`,
-          role: 'member',
-          isGuest: false,
-        });
+          ...userDoc.data(),
+        } as any);
       } else {
         console.log('No current user found.');
       }
     } catch (error: Error | any) {
       if (error.code == 'auth/invalid-verification-code') {
-        console.log('Invalid code.');
+        Alert.alert('Código inválido.');
+      } else if (error.code == 'auth/credential-already-in-use') {
+        Alert.alert('Error de vinculación de cuenta', error.message);
       } else {
-        console.log('Account linking error');
+        Alert.alert('Error al verificar el código', error.message);
       }
+      console.error('Error confirming code:', error);
+    } finally {
+      navigation.navigate('TabNavigator');
     }
   }
 
@@ -58,17 +71,28 @@ const PhoneVerificationScreen: React.FC = () => {
       alignItems="center"
       padding={20}
       backgroundColor={theme.background}>
-      <SizableText fontSize={24} marginBottom={20} color={theme.color}>
-        Verificación de Teléfono
-      </SizableText>
-      <SizableText fontSize={16} textAlign="center" marginBottom={40} color={theme.color}>
-        Se enviará un código de verificación a su número de teléfono: {params.phoneNumber}.
+      <SizableText
+        fontSize={'$8'}
+        textAlign="center"
+        lineHeight={'$8'}
+        marginBottom={20}
+        color={theme.text.get()}>
+        Vamos a verificar su número de teléfono
       </SizableText>
 
       {!confirm ? (
-        <PrimaryButton onPress={() => handlePhoneNumberVerification(params.phoneNumber)}>
-          Enviar Código
-        </PrimaryButton>
+        <>
+          <SizableText
+            fontSize={'$6'}
+            textAlign="center"
+            marginBottom={40}
+            color={theme.text.get()}>
+            Se enviará un código a su número de teléfono: {params.phoneNumber}.
+          </SizableText>
+          <PrimaryButton px="$8" onPress={() => handlePhoneNumberVerification(params.phoneNumber)}>
+            Enviar Código
+          </PrimaryButton>
+        </>
       ) : (
         <YStack flex={1} alignItems="center" gap={'$4'}>
           <SizableText fontSize={16} marginBottom={20} color={theme.color}>
@@ -84,7 +108,10 @@ const PhoneVerificationScreen: React.FC = () => {
             style={{ letterSpacing: 12, textAlign: 'center', fontSize: 32 }}
             variant="primary"
           />
-          <PrimaryButton onPress={confirmCode} disabled={confirm === null || code.length === 0}>
+          <PrimaryButton
+            px="$8"
+            onPress={confirmCode}
+            disabled={confirm === null || code.length === 0}>
             Confirmar Código
           </PrimaryButton>
         </YStack>
